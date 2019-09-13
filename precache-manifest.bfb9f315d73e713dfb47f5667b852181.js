3e8ff5a900f2b80f7049558157477941b919409a
Jo <jdheinen@yahoo.com>
2019-09-13 19:41:42 +1000

    Updates

---
+++ precache-manifest.bfb9f315d73e713dfb47f5667b852181.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bec1e75d8325c75db56d27a3a55b7d37",
+    "url": "/portoplants/index.html"
+  },
+  {
+    "revision": "903fd3d2d838dfa6e5ce",
+    "url": "/portoplants/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "997516161a5e8b1369e3",
+    "url": "/portoplants/static/css/main.0aaa6ff1.chunk.css"
+  },
+  {
+    "revision": "903fd3d2d838dfa6e5ce",
+    "url": "/portoplants/static/js/2.cea8f345.chunk.js"
+  },
+  {
+    "revision": "997516161a5e8b1369e3",
+    "url": "/portoplants/static/js/main.d8824407.chunk.js"
+  },
+  {
+    "revision": "93372e4f0a3c20a50c8a",
+    "url": "/portoplants/static/js/runtime~main.b276a4c4.js"
+  },
+  {
+    "revision": "81b4d02f7c9a30b89edaafafa329db46",
+    "url": "/portoplants/static/media/about-img.81b4d02f.jpg"
+  },
+  {
+    "revision": "bbcf348f0c3d9ae2ef6b346012cc5bdc",
+    "url": "/portoplants/static/media/call-to-action-bg.bbcf348f.jpg"
+  },
+  {
+    "revision": "30071f8066c48fa841ebfa0cb4ef79fe",
+    "url": "/portoplants/static/media/mainnur.30071f80.jpg"
+  }
+]);
